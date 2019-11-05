13cc1f382ac0bf1bed7bdee9c9d79b531941f23a
ilya <shefchenkornd@gmail.com>
2019-11-05 09:57:39 +0300

    Добавил файлы от Гоши Дударь

--- read_status.go
+++ read_status.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"bufio"
 )
 
-func main() {
-	conn, _ := net.Dial("tcp", "goland.org:80")
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+func main()  {
+	conn, _ := net.Dial("tcp", "golang.org:443")
+
+	fmt.Println(conn, 1)
+
+	//fmt.Fprintf(conn, "GET / HTTP/2\r\n\r\n")
+
 	status, _ := bufio.NewReader(conn).ReadString('\n')
+
 	fmt.Println(status)
 }
