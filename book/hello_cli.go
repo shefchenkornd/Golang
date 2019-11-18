86f46a6deb22c69b2581b8f18e4a3f8a30d12b86
ilya <shefchenkornd@gmail.com>
2019-11-18 11:53:52 +0300

    Добавил флаги в программу

--- book/hello_cli.go
+++ book/hello_cli.go
@@ -13,9 +13,23 @@ func main() {
 	app := cli.App{
 		Name: "hello_cli",
 		Usage: "Who to say hello to",
+		Flags: []cli.Flag {
+			&cli.StringFlag{
+				Name:        "name, n",
+				Usage:       "Who to say hello to",
+				Value:       "World",
+			},
+		},
 		Action: func(c *cli.Context) error {
 			// name := c.GlobalString("name")
 			name := "John"
+
+			fmt.Println(c.NArg())
+
+			if c.NArg() > 0  {
+				name = c.Args().Get(0)
+			}
+
 			fmt.Printf("Hello, %s\n", name)
 			return nil
 		},
