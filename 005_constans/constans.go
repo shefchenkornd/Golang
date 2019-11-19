3fc3e4d14a8c610d2ee0c6e489976a2d83786476
ilya <shefchenkornd@gmail.com>
2019-11-19 23:28:57 +0300

    contst

---
+++ 005_constans/constans.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const APPLICATIONNAME = "Gofer"
+
+const (
+	A = "Alfa"
+	B = "Bmw"
+	C = "Citroen"
+)
+
+func main() {
+	fmt.Println(APPLICATIONNAME)
+
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+}
